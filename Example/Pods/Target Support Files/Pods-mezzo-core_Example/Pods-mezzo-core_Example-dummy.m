c56deb918b6a30271ada9b2adc28844c8e3715df
t0tep <christopher.castillo@mvp.net.ph>
2020-04-02 13:39:18 +0800

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-mezzo-core_Example/Pods-mezzo-core_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mezzo_core_Example : NSObject
+@end
+@implementation PodsDummy_Pods_mezzo_core_Example
+@end
---
+++ Example/Pods/Target Support Files/mezzo-core/mezzo-core-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_mezzo_core : NSObject
+@end
+@implementation PodsDummy_mezzo_core
+@end
